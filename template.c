803915aae93190ac8c90f03911279f95c260fe0a
Nayan Shah <nayanmange@gmail.com>
2012-04-08 23:40:11 +0530

    C, C++, Java templates added.

---
+++ template.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    int t, n;
+    scanf("%d", &t);
+    while (t-- > 0) {
+        scanf("%d", &n);
+
+        printf("%d\n", n);
+    }
+    return 0;
+}
---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char *argv[]) {
+
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+
+        cout << n << endl;
+    }
+    return 0;
+}
